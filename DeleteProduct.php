a68d4d553a7255d80d654fc9fe37fd3903cc59d6
thaiCEO <kimthaibmc2017@gmail.com>
2025-05-14 19:47:24 +0700

    edit

--- DeleteProduct.php
+++ DeleteProduct.php
@@ -1,5 +1,5 @@
 <?php 
-include 'config.php';
+include 'db/config.php';
 
 $id = $_GET['id'];
 
--- ProductList.php
+++ ProductList.php
@@ -1,5 +1,5 @@
 <?php 
-include 'config.php';
+include 'db/config.php';
 include 'components/header.php';
 include 'components/navbar.php';
 include 'components/sidebar.php';
